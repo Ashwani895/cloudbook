900a19afd51a590ecd6982a7089919af6702a452
Ashwani Mishra <ashwanimishraxyz@gmail.com>
2026-02-24 03:52:54 +0530

    Added health check route

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,27 @@
-require('dotenv').config(); // add this at top if using .env
-
-const connectToMongo = require('./db');
-connectToMongo();
+require('dotenv').config();
 
 const express = require('express');
 const cors = require('cors');
+const connectToMongo = require('./db');
+
+connectToMongo();
 
 const app = express();
 
+
 const port = process.env.PORT || 8000;
 
-app.use(cors());
+
+app.use(cors({
+  origin: "*",   
+}));
+
 app.use(express.json());
+
+
+app.get("/", (req, res) => {
+  res.status(200).send("Cloudbook Backend Running 🚀");
+});
 
 // Routes
 app.use('/api/auth', require('./routes/auth'));
